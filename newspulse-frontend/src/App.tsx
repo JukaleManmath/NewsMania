4855a9f6ab96eb0afbe74e646a22ae9514099770
Manmath Jukale <jukalemanmath@gmail.com>
2025-03-16 18:57:20 -0400

    Redis cache implemented and created frontend file structure

---
+++ newspulse-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="bg-blue-500 text-white text-3xl font-bold p-4">
+    🚀 NewsPulse with Tailwind + Vite!
+    </div>
+  )
+}
+
+export default App
